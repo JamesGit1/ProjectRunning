a19b3edb5d0916a384fbba7a1e7d5c557bb9f849
JamesGit1 <jamesshaw112@sky.com>
2021-06-01 18:37:50 +0100

    Darkmode toggler is work in progress

--- myApp/src/pages/Tab3.tsx
+++ myApp/src/pages/Tab3.tsx
@@ -3,29 +3,6 @@ import ExploreContainer from '../components/ExploreContainer';
 import './Tab3.css';
 import React, { useState } from 'react';
 
-// Query for the toggle that is used to change between themes
-const toggle = document.querySelector('#themeToggle');
-
-// Listen for the toggle check/uncheck to toggle the dark class on the <body>
-toggle.addEventListener('ionChange', (ev) => {
-  document.body.classList.toggle('dark', ev.detail.checked);
-});
-
-const prefersDark = window.matchMedia('(prefers-color-scheme: dark)');
-
-// Listen for changes to the prefers-color-scheme media query
-prefersDark.addListener((e) => checkToggle(e.matches));
-
-// Called when the app loads
-function loadApp() {
-  checkToggle(prefersDark.matches);
-}
-
-// Called by the media query to check/uncheck the toggle
-function checkToggle(shouldCheck) {
-  toggle.checked = shouldCheck;
-}
-
 const Tab3: React.FC = () => {
   const [checked, setChecked] = useState(false);
   return (
@@ -45,13 +22,18 @@ const Tab3: React.FC = () => {
 
         <IonItem>
           <IonLabel>Checked: {JSON.stringify(checked)}</IonLabel>
-          <IonToggle checked={checked} onIonChange={e => setChecked(e.detail.checked)} />
+          <IonToggle checked={checked} onIonChange={toggleDarkModeHandler} />
         </IonItem>
 
 
       </IonContent>
     </IonPage>
   );
+};
+
+const toggleDarkModeHandler = () => {
+  //e => setChecked(e.detail.checked)
+  document.body.classList.toggle("dark");
 };
 
 export default Tab3;
