755a2498aedf54235f3b213089400e20c02c9a60
JamesGit1 <jamesshaw112@sky.com>
2021-06-29 16:06:59 +0100

    You can delete photos now

--- myApp/src/pages/Tab2.tsx
+++ myApp/src/pages/Tab2.tsx
@@ -1,15 +1,19 @@
+import { usePhotoGallery, UserPhoto } from '../hooks/usePhotoGallery';
 import { camera, trash, close } from 'ionicons/icons';
 import {
   IonContent, IonHeader, IonPage, IonTitle, IonToolbar,
   IonFab, IonFabButton, IonIcon, IonGrid, IonRow,
   IonCol, IonImg, IonActionSheet
 } from '@ionic/react';
-import { usePhotoGallery } from "../hooks/usePhotoGallery";
+
 import ExploreContainer from '../components/ExploreContainer';
 import './Tab2.css';
+import { useState } from 'react';
 
 const Tab2: React.FC = () => {
-  const { photos, takePhoto } = usePhotoGallery();
+  // Photo References
+  const { photos, takePhoto, deletePhoto } = usePhotoGallery();
+  const [photoToDelete, setPhotoToDelete] = useState<UserPhoto>();
 
   return (
     <IonPage>
@@ -23,21 +27,42 @@ const Tab2: React.FC = () => {
           <IonRow>
             {photos.map((photo, index) => (
               <IonCol size="6" key={index}>
-                <IonImg src={photo.webviewPath} />
+                <IonImg onClick={() => setPhotoToDelete(photo)}
+                  src={photo.webviewPath} />
               </IonCol>
             ))}
           </IonRow>
         </IonGrid>
         {/* <IonFab> markup */}
-          <IonFab vertical="bottom" horizontal="center" slot="fixed">
-            <IonFabButton onClick={() => takePhoto()}>
-              <IonIcon icon={camera}></IonIcon>
-            </IonFabButton>
-          </IonFab>
+        <IonFab vertical="bottom" horizontal="center" slot="fixed">
+          <IonFabButton onClick={() => takePhoto()}>
+            <IonIcon icon={camera}></IonIcon>
+          </IonFabButton>
+        </IonFab>
 
+        {/* Action sheet to popup allowing deletion */}
+        <IonActionSheet
+          isOpen={!!photoToDelete}
+          buttons={[{
+            text: 'Delete',
+            role: 'destructive',
+            icon: trash,
+            handler: () => {
+              if (photoToDelete) {
+                deletePhoto(photoToDelete);
+                setPhotoToDelete(undefined);
+              }
+            }
+          }, {
+            text: 'Cancel',
+            icon: close,
+            role: 'cancel'
+          }]}
+          onDidDismiss={() => setPhotoToDelete(undefined)}
+        />
       </IonContent>
     </IonPage>
-      );
+  );
 };
 
-      export default Tab2;
+export default Tab2;
