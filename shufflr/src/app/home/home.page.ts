b4a038668b0737e4018ad0f2dce6399a7d6d9ea7
James <jamesshaw112@sky.com>
2021-07-17 17:07:57 +0100

    Adding Angular version to branch

---
+++ shufflr/src/app/home/home.page.ts
@@ -0,0 +1,30 @@
+import { Component } from '@angular/core';
+import { NavController } from '@ionic/angular';
+ 
+declare var cordova: any;
+ 
+@Component({
+  selector: 'app-home',
+  templateUrl: 'home.page.html',
+  styleUrls: ['home.page.scss'],
+})
+export class HomePage {
+  result = {};
+ 
+  constructor(public navCtrl: NavController) { }
+ 
+  authWithSpotify() {
+    const config = {
+      clientId: "6875299652d04e53a7067cf12fa0d627",
+      redirectUrl: "shufflr://callback",
+      scopes: ["streaming", "playlist-read-private", "user-read-email", "user-read-private"],
+      tokenExchangeUrl: "https://shufflr-spotify.herokuapp.com/exchange",
+      tokenRefreshUrl: "https://shufflr-spotify.herokuapp.com/refresh",
+    };
+ 
+    cordova.plugins.spotifyAuth.authorize(config)
+      .then(({ accessToken, encryptedRefreshToken, expiresAt }) => {
+        this.result = { access_token: accessToken, expires_in: expiresAt, ref: encryptedRefreshToken };
+      });
+  }
+}
