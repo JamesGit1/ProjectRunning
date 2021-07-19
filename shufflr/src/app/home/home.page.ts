a803950b7195e50d72ba7ae042bea848a196919a
James <jamesshaw112@sky.com>
2021-07-19 16:19:48 +0100

    Removed some unessisarry things

--- shufflr/src/app/home/home.page.ts
+++ shufflr/src/app/home/home.page.ts
@@ -1,8 +1,8 @@
 import { Component } from '@angular/core';
 import { NavController } from '@ionic/angular';
- 
-declare var cordova: any;
- 
+
+declare let cordova: any;
+
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
@@ -10,21 +10,31 @@ declare var cordova: any;
 })
 export class HomePage {
   result = {};
- 
-  constructor(public navCtrl: NavController) { }
- 
+
+  constructor(public navCtrl: NavController) {}
+
   authWithSpotify() {
     const config = {
-      clientId: "6875299652d04e53a7067cf12fa0d627",
-      redirectUrl: "shufflr://callback",
-      scopes: ["streaming", "playlist-read-private", "user-read-email", "user-read-private"],
-      tokenExchangeUrl: "https://shufflr-spotify.herokuapp.com/exchange",
-      tokenRefreshUrl: "https://shufflr-spotify.herokuapp.com/refresh",
+      clientId: '6875299652d04e53a7067cf12fa0d627',
+      redirectUrl: 'shufflr://callback',
+      scopes: [
+        'streaming',
+        'playlist-read-private',
+        'user-read-email',
+        'user-read-private',
+      ],
+      tokenExchangeUrl: 'https://shufflr-spotify.herokuapp.com/exchange',
+      tokenRefreshUrl: 'https://shufflr-spotify.herokuapp.com/refresh',
     };
- 
-    cordova.plugins.spotifyAuth.authorize(config)
+
+    cordova.plugins.spotifyAuth
+      .authorize(config)
       .then(({ accessToken, encryptedRefreshToken, expiresAt }) => {
-        this.result = { access_token: accessToken, expires_in: expiresAt, ref: encryptedRefreshToken };
+        this.result = {
+          accessTokenResult: accessToken,
+          expiresAtResult: expiresAt,
+          ref: encryptedRefreshToken,
+        };
       });
   }
 }
